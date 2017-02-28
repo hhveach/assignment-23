7f35c905672420b2e4a660c6b0c18e341a1ef3df
Henry Veach III <hhveach@Henrys-MacBook-Pro.local>
2017-02-28 15:41:41 -0500

    checkpoint

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,45 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 
-const ProfileList = React.createClass({
+const CongMainList = React.createClass({
+
   render: function(){
     return (
       <div className="column-container">
-        <SingleList congList={this.props.mainList}/>
+        <input className="zip-code" type="text" placeholder="Enter ZIP" />
+        {this.props.mainList.map(function(listEl){
+          return <CreateCongList senInfo={listEl}/>;
+        })};
       </div>
     )
   }
 });
 
-const SingleList = React.createClass({
-    createCongInfo: function(arrOfObj){
-      let jsxArray = arrOfObj.map(function(listEl){
-        return <div className="main-list">
-                <h2>{listEl.first_name} {listEl.last_name}</h2>
-                <h2>{listEl.title} --{listEl.party}-{listEl.state_name}</h2>
-                <ul>
-                  <li>{listEl.oc_email}</li>
-                  <li>{listEl.website}</li>
-                  <li>{listEl.facebook_id}</li>
-                  <li>{listEl.twitter_id}</li>
-                </ul>
-                <p>{listEl.term_end}</p>
-              </div>
-      });
-      return jsxArray;
-    },
-
+const CreateCongList = React.createClass({
 
     render: function(){
-      let congressList = this.props.congList;
       return (
-            <div class="main-list">
-              {this.createCongInfo(congressList)}
+            <div className="main-list">
+              <h2>{this.props.senInfo.first_name} {this.props.senInfo.last_name}</h2>
+              <h2>{this.props.senInfo.title} --{this.props.senInfo.party}-{this.props.senInfo.state_name}</h2>
+              <ul>
+                <li>email: {this.props.senInfo.oc_email}</li>
+                <li>website: {this.props.senInfo.website}</li>
+                <li>facebook: {this.props.senInfo.facebook_id}</li>
+                <li>twitter: {this.props.senInfo.twitter_id}</li>
+              </ul>
+              <p>{this.props.senInfo.term_end}</p>
             </div>
             )
         }
 });
 
+// const ZipCode = React.createClass({
+//   inputSubmit: function(evt){
+//
+//   }
+//
+// });
+
 $.getJSON('https://congress.api.sunlightfoundation.com/legislators?callback=?').then(function(serverRes){
-  ReactDOM.render( <ProfileList mainList={serverRes.results}/>, document.querySelector('#app-container'));
+  ReactDOM.render( <CongMainList mainList={serverRes.results}/>, document.querySelector('#app-container'));
 });
