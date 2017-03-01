1cc4b95a4c3db8ed9d5b2d04cbf0a37e3f8af917
Henry Veach III <hhveach@Henrys-MacBook-Pro.local>
2017-03-01 11:32:47 -0500

    initial

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,10 +5,11 @@ import $ from 'jquery';
 const CongMainList = React.createClass({
 
   render: function(){
+    const { mainList } = this.props;
     return (
       <div className="column-container">
-        <input className="zip-code" type="text" placeholder="Enter ZIP" />
-        {this.props.mainList.map(function(listEl){
+        <input className="zip-code" type="text" placeholder="Search Legislators by ZIP" />
+        {mainList.map(function(listEl){
           return <CreateCongList senInfo={listEl}/>;
         })};
       </div>
@@ -19,28 +20,22 @@ const CongMainList = React.createClass({
 const CreateCongList = React.createClass({
 
     render: function(){
+      const {first_name, last_name, title, party, state_name, oc_email, website, facebook_id, twitter_id, term_end } = this.props.senInfo;
       return (
             <div className="main-list">
-              <h2>{this.props.senInfo.first_name} {this.props.senInfo.last_name}</h2>
-              <h2>{this.props.senInfo.title} --{this.props.senInfo.party}-{this.props.senInfo.state_name}</h2>
+              <h2>{first_name} {last_name}</h2>
+              <h2>{title} --{party}-{state_name}</h2>
               <ul>
-                <li>email: {this.props.senInfo.oc_email}</li>
-                <li>website: {this.props.senInfo.website}</li>
-                <li>facebook: {this.props.senInfo.facebook_id}</li>
-                <li>twitter: {this.props.senInfo.twitter_id}</li>
+                <li>email: {oc_email}</li>
+                <li>website: {website}</li>
+                <li>facebook: {facebook_id}</li>
+                <li>twitter: {twitter_id}</li>
               </ul>
-              <p>{this.props.senInfo.term_end}</p>
+              <p>Term end: {term_end}</p>
             </div>
             )
         }
 });
-
-// const ZipCode = React.createClass({
-//   inputSubmit: function(evt){
-//
-//   }
-//
-// });
 
 $.getJSON('https://congress.api.sunlightfoundation.com/legislators?callback=?').then(function(serverRes){
   ReactDOM.render( <CongMainList mainList={serverRes.results}/>, document.querySelector('#app-container'));
